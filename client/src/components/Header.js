0d33419d429edebcb7a7d2ef740d4a6f11e4d9e2
Yaroslav Himko <yaroslav.himko@gmail.com>
2020-06-13 15:29:08 +0300

    Added routes to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,39 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
+    console.log(this.props);
     return (
       <div>
         <nav>
           <div className="nav-wrapper">
-            <a className="left brand-logo">Node-with-react</a>
-            <ul className="right">
-              <li>
-                <a> Login with Google</a>
-              </li>
-            </ul>
+            <Link
+              to={this.props.auth ? "/surveys" : "/"}
+              className="left brand-logo"
+            >
+              Node-with-react
+            </Link>
+            <ul className="right">{this.renderContent()}</ul>
           </div>
         </nav>
       </div>
@@ -19,4 +41,10 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return {
+    auth,
+  };
+}
+
+export default connect(mapStateToProps)(Header);
