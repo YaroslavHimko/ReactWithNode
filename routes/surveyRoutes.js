2eeb49b0c77605f15a6a4307ab69460285d2c4c9
Yaroslav Himko <yaroslav.himko@gmail.com>
2020-06-27 02:52:31 +0300

    Attempt to fix redirection issue

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -22,7 +22,7 @@ module.exports = (app) => {
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    const p = new Path("/api/surveys/:surveyId/:choice");
+    const p = new Path("//api/surveys/:surveyId/:choice");
     const { signature, "event-data": eventData } = req.body;
 
     _.chain(eventData)
